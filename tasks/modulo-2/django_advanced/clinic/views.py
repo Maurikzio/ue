c14d8b4e899f43f7acb43008db7724e40f53f7bc
Mauricio <m.morocho1196@gmail.com>
2025-02-08 18:25:51 -0500

    Added views and urls for services

--- tasks/modulo-2/django_advanced/clinic/views.py
+++ tasks/modulo-2/django_advanced/clinic/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ServiceSerializer
+from .models import Service
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+
+class ServiceViewSet(viewsets.ModelViewSet):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+    permission_classes = [AllowAny]
