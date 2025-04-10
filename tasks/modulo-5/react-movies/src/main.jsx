d828713936f3794b234cc1272574b3aaf98ec6f0
Mauricio <m.morocho1196@gmail.com>
2025-04-10 12:37:57 -0500

    Created 404

--- tasks/modulo-5/react-movies/src/main.jsx
+++ tasks/modulo-5/react-movies/src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from "react-router";
 import App from './App.jsx';
+import NotFoundPage from './pages/NotFoundPage.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   </BrowserRouter>,
 );
---
+++ tasks/modulo-5/react-movies/src/pages/NotFoundPage.jsx
@@ -0,0 +1,26 @@
+export default function NotFoundPage() {
+  return (
+    <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <p className="text-base font-semibold text-green-600">404</p>
+        <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">
+          Pagina no econtrada :/
+        </h1>
+        <p className="mt-6 text-lg font-medium text-pretty text-gray-200 sm:text-xl/8">
+          Lo sentimos, no podemos encontrar la pagina que estas buscando.
+        </p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <a
+            href="#"
+            className="rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-green-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+          >
+            Go back home
+          </a>
+          <a href="#" className="text-sm font-semibold text-white-900">
+            Contact support <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div>
+      </div>
+    </main>
+  );
+}
