f538bbc8026dc3d2e3ab1ae7d4d6eafc8282b8ca
Mauricio <m.morocho1196@gmail.com>
2025-04-09 22:31:26 -0500

    Initial setup

---
+++ tasks/modulo-5/react-movies/src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react';
+// import viteLogo from '/vite.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline bg-red-500">
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
