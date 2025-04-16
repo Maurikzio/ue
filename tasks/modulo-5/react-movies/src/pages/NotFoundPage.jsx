3bd6533237e3bfab41ad1310992a6313b328788d
Mauricio <m.morocho1196@gmail.com>
2025-04-15 21:03:30 -0500

    Fixed 404 page

--- tasks/modulo-5/react-movies/src/pages/NotFoundPage.jsx
+++ tasks/modulo-5/react-movies/src/pages/NotFoundPage.jsx
@@ -1,21 +1,23 @@
+import { Link } from "react-router";
+
 export default function NotFoundPage() {
   return (
     <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
-        <p className="text-base font-semibold text-green-600">404</p>
+        <p className="text-base font-semibold text-red-600">404</p>
         <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">
-          Pagina no econtrada :/
+          Page not found
         </h1>
         <p className="mt-6 text-lg font-medium text-pretty text-gray-200 sm:text-xl/8">
-          Lo sentimos, no podemos encontrar la pagina que estas buscando.
+          Sorry, we couldn't find the page you're looking for.
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
-          <a
-            href="#"
-            className="rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-green-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+          <Link
+            to="/"
+            className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-red-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
           >
             Go back home
-          </a>
+          </Link>
           <a href="#" className="text-sm font-semibold text-white-900">
             Contact support <span aria-hidden="true">&rarr;</span>
           </a>
